7161303c75fd9150d6ea654172c55e49cd73fa81
Harsh Sharma <43477089+Harsharma1210@users.noreply.github.com>
2023-08-16 20:19:56 -0400

    Added More Instructions

--- Cars.sql
+++ Cars.sql
@@ -1,11 +1,24 @@
 CREATE DATABASE IF NOT EXISTS Cars;
 USE Cars;
 
-DROP TABLE IF EXISTS buyer;
-CREATE TABLE buyer (
-	username varchar(50),
+DROP TABLE IF EXISTS customerRep;
+CREATE TABLE customerRep (
+	id int primary key,
+    username varchar(50),
     password varchar(50)
 );
 
-INSERT INTO buyer
-VALUES ('root', 'root');
+DROP TABLE IF EXISTS buyerSeller;
+CREATE TABLE buyerSeller (
+	buyerid int primary key,
+    username varchar(50),
+    password varchar(50)
+);
+
+INSERT INTO customerRep VALUES
+(1, "bob", "parr");
+
+INSERT INTO buyerSeller VALUES
+(1, "seto", "kaiba");
+
+show tables;
